7fd3248a443572de473d3f452fcb92622c5f62e9
João Gabriel Perez <104282903+JoaoGperez@users.noreply.github.com>
2024-08-12 16:09:45 -0300

    sobre

---
+++ assests/js/inicio.js
@@ -0,0 +1,29 @@
+// inicio.js
+
+export function initTypingEffect() {
+    const typingText = document.getElementById('typing-text');
+    const fullText = typingText.textContent;
+    typingText.textContent = ''; // Limpa o texto inicial
+
+    let charIndex = 0;
+
+    // Função para adicionar letras ao texto
+    const typeLetter = () => {
+        if (charIndex < fullText.length) {
+            typingText.textContent += fullText[charIndex];
+            charIndex++;
+            setTimeout(typeLetter, 150); // Tempo entre cada letra
+        }
+    };
+
+    // Inicia a animação de digitação após um breve atraso para sincronizar com o cursor
+    setTimeout(() => {
+        typingText.style.visibility = 'visible';
+        typeLetter();
+    }, 100); // Atraso para sincronizar com o CSS
+}
+
+// Chama a função quando o DOM estiver pronto
+document.addEventListener('DOMContentLoaded', () => {
+    initTypingEffect();
+});
--- assests/js/main.js
+++ assests/js/main.js
@@ -1,8 +1,12 @@
 // main.js
 
 import { setupMenu } from './menu.js';
+import { initTypingEffect } from './inicio.js';
+
 
 // Inicializa o menu quando o documento estiver carregado
 document.addEventListener('DOMContentLoaded', function() {
     setupMenu();
+    initTypingEffect();
+
 });
