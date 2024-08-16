4af863815f65f7190857c63e0e27e9d6778e8da8
João Gabriel Perez <104282903+JoaoGperez@users.noreply.github.com>
2024-08-15 23:07:35 -0300

    ajustes

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,12 +1,14 @@
 // main.js
 
 import { setupMenu } from './menu.js';
+import { smoothScroll } from './menu.js';
 import { initTypingEffect } from './inicio.js';
 import { toggleProjects } from './projetos.js';
 
 // Inicializa o menu quando o documento estiver carregado
 document.addEventListener('DOMContentLoaded', function() {
     setupMenu();
+    smoothScroll();
     initTypingEffect();
     toggleProjects();
 
--- assests/js/menu.js
+++ assests/js/menu.js
@@ -8,3 +8,26 @@ export function setupMenu() {
         navLinks.classList.toggle('active');
     });
 }
+
+// smoothScroll.js
+export function smoothScroll() {
+    const links = document.querySelectorAll('.nav-links a');
+
+    links.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault(); // Impede o comportamento padrão do link
+            const targetId = this.getAttribute('href').substring(1); // Obtém o ID do alvo
+            const targetElement = document.getElementById(targetId);
+            
+            // Defina a compensação com base na altura do seu menu ou header fixo
+            const offset = 80; // Ajuste este valor conforme necessário
+
+            const elementPosition = targetElement.offsetTop - offset;
+
+            window.scrollTo({
+                top: elementPosition,
+                behavior: 'smooth'
+            });
+        });
+    });
+}
